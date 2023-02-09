0dfd8b6179daa3b9839bcf3123787b77927b5f64
Charan <gcharankumar99@gmail.com>
2023-02-09 12:47:33 -0600

    vars file

--- 08-loops/count/main.tf
+++ 08-loops/count/main.tf
@@ -17,8 +17,8 @@ data "aws_ami" "centos8" {
 
 output "publicip" {
   # without count
-  value = aws_instance.web.public_ip
+  #value = aws_instance.web.public_ip
 
   # with count
   value = aws_instance.web.*.public_ip
-  }
+}
