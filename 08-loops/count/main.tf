8509f752356173b011122ba9a132d221d0c5db13
Charan <gcharankumar99@gmail.com>
2023-02-09 12:38:51 -0600

    vars file

--- 08-loops/count/main.tf
+++ 08-loops/count/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  count         = 2
+
   ami           = data.aws_ami.centos8.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
 
   tags = {
     Name = "test-centos8"
