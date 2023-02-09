4451ea06c4feb6d8b41e0af0531dca7c3fddbc90
Charan <gcharankumar99@gmail.com>
2023-02-08 23:24:46 -0600

    vars file

---
+++ 06-modules/modules.tf
@@ -0,0 +1,17 @@
+module "sg" {
+  source = "./sg"
+}
+
+module "ec2" {
+  source = "./ec2"
+  security_group_id = module.sg.security_group_id
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-c93"
+    key    = "05-remote-state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
