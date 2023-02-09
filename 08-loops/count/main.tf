d10a9ed08ddff331f1a357e917ef914f20a45f19
Charan <gcharankumar99@gmail.com>
2023-02-09 13:12:55 -0600

    vars file

--- 08-loops/count/main.tf
+++ 08-loops/count/main.tf
@@ -1,11 +1,10 @@
 resource "aws_instance" "web" {
-  count         = 1
+  count         = length(var.components)
   ami           = data.aws_ami.centos8.id
   instance_type = "t3.micro"
 
-
   tags = {
-    Name = "test-centos8"
+    Name = "var.components"
   }
 }
 
@@ -21,4 +20,8 @@ output "publicip" {
 
   # with count
   value = aws_instance.web.*.public_ip
+}
+
+variable "components" {
+  default = ["cart", "catalogue"]
 }
