17a623042c43bf01a2e3edce36c1849af4e7f8f9
Charan <gcharankumar99@gmail.com>
2023-02-08 23:04:59 -0600

    vars file

--- 05-remote-state/sample.tf
+++ 05-remote-state/sample.tf
@@ -29,7 +29,6 @@ terraform {
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
 
   ingress {
     description      = "TLS from VPC"
