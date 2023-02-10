76bf829606bdf205144d2bf629ff642a95223d79
Charan <gcharankumar99@gmail.com>
2023-02-10 13:31:06 -0600

    vars file

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -9,8 +9,8 @@ data "aws_ami" "centos8" {
 }
 
 resource "aws_instance" "web" {
-  ami                   = data.aws_ami.centos8.id
-  instance_type         = "t3.micro"
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
@@ -31,6 +31,7 @@ resource "null_resource" "provision" {
     ]
   }
 }
+
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
